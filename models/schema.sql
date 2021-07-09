2f39cbed9650a25a6c2fbea82c0723aeee7c7102
Kiran Kanda <kkanda@dons.usfca.edu>
2021-07-09 15:20:03 -0700

    created and updated schema.sql and seeds.sql

---
+++ models/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,0) NOT NULL,
+department_id INT,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+id INT AUTO_INCREMENT NOT NULL,
+department_name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Harry', 'Styles', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Conrad', 'James', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Zendaya', 'Lewis', 1, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Spencer', 'Shay', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Suhani', 'Win', 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Nick', 'Call', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Gabe', 'Sterning', 4, 1);
+
+INSERT INTO department (department_name)
+VALUES ('Engineering');
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Business Development');
+INSERT INTO department (department_name)
+VALUES ('Design');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Software Designer', 100000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Executive Assistant', 200000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ('CEO', 300000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Public Relations Manager', 150000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Creative Director', 200000, 5);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Technical Director', 250000, 2);
